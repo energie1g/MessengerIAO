9b89ff9579d99264b356fb9608b40b09f7579789
Badr Benchagra <badr.redax@gmail.com>
2019-01-19 14:29:44 +0100

    I added login authentication (not completed)

--- app/src/main/java/world/iaomessenger/LoginActivity.java
+++ app/src/main/java/world/iaomessenger/LoginActivity.java
@@ -2,14 +2,22 @@ package world.iaomessenger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.TextureView;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
@@ -21,18 +29,24 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginEmail, loginPassword;
     private Button loginBtn;
     private TextView needRegister, forgetPassword;
+    private String lEmail, lPassword;
+
+    private FirebaseAuth currAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        currAuth = FirebaseAuth.getInstance();
+
         getFieldByIds();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                logIn();
+                logIn();
             }
         });
 
@@ -49,6 +63,52 @@ public class LoginActivity extends AppCompatActivity {
                 redirectUserToForgetPasswordActivity();
             }
         });
+
+        loginPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginBtn.performClick();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void logIn() {
+        lEmail = loginEmail.getText().toString();
+        lPassword = loginPassword.getText().toString();
+
+        boolean fieldsAreEmpty = checkIfLoginFieldsAreEmpty(lEmail, lPassword);
+
+        if(!fieldsAreEmpty) {
+            currAuth.signInWithEmailAndPassword(lEmail, lPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        redirectUserToMainActivity();
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean checkIfLoginFieldsAreEmpty(String email, String pwd) {
+
+        if(TextUtils.isEmpty(email)) {
+            loginEmail.setError("Email is required");
+            return true;
+        }
+
+        if(TextUtils.isEmpty(pwd)) {
+            loginPassword.setError("Password is required");
+            return true;
+        }
+
+        return false;
     }
 
     private void getFieldByIds() {
