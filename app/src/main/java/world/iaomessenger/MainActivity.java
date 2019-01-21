02450645bffffaa6b3a00d6aaf74e569f4b3932f
Badr Benchagra <badr.redax@gmail.com>
2019-01-21 17:10:35 +0100

    Update: none

--- app/src/main/java/world/iaomessenger/MainActivity.java
+++ app/src/main/java/world/iaomessenger/MainActivity.java
@@ -1,16 +1,25 @@
 package world.iaomessenger;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private DatabaseReference databaseReference;
 
     private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
     private ViewPager mViewPager;
+
+    private CoordinatorLayout coordinatorLayoutMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +43,16 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         Log.d(TAG, "onCreate: STARTED.");
 
         mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
+
+        coordinatorLayoutMain = (CoordinatorLayout) findViewById(R.id.coordinatorLayoutMain);
+
         setupViewPager(mViewPager);
     }
 
@@ -45,7 +62,41 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser == null) {
             Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(loginIntent);
+        } else {
+            VerifyUserExistense();
         }
+    }
+
+    private void VerifyUserExistense() {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+        databaseReference.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("username").exists()) {
+
+                    // TODO: 1/21/2019 ADD the user's name (after the "Welcome").
+                    Snackbar welcomeSnackbar = Snackbar.make(coordinatorLayoutMain, "Welcome!", Snackbar.LENGTH_LONG);
+                    welcomeSnackbar.setAction("CLOSE", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                        }
+                    });
+                    welcomeSnackbar.setActionTextColor(Color.WHITE);
+                    welcomeSnackbar.show();
+
+                } else {
+
+                    // TODO: 1/21/2019 Send the user to the settingsActivity to set his username (required) 
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void setupViewPager(ViewPager viewPager){
